c741d0203dcdbcf4ee37254269b4e838c9534580
xgg66 <444712055@qq.com>
2019-05-06 09:41:01 +0800

    项目

---
+++ shop/js/register.js
@@ -0,0 +1,32 @@
+class Register {
+  constructor () {
+    this.inputUserName = document.querySelector("#inputUserName");
+    this.inputPassword = document.querySelector("#inputPassword");
+    this.btn = document.querySelector("#btn");
+    this.bindEvent();
+  }
+
+  bindEvent () {
+    this.btn.onclick = () => {
+      let username = this.inputUserName.value,
+          password = this.inputPassword.value;
+      
+      // 数据验证
+      // 发送请求
+      tools.ajax("POST", "../api/v1/register.php", {username, password}, data => {
+      	
+      		
+      		alert("注册成功");
+      	
+        
+      })
+
+      return false;
+
+    }
+  }
+
+
+}
+
+new Register();
